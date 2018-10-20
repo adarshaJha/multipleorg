0a3742066b237bc51ca0707c0afbfe06ff6c0055
Prashant Prakash <oracle.prashantprakash@gmail.com>
2018-10-20 22:51:18 +0530

    Update startFabric.sh

--- demo-app/startFabric.sh
+++ demo-app/startFabric.sh
@@ -34,7 +34,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/g
 sleep 10
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C ppchannel -n demo-app -c '{"function":"initLedger","Args":[""]}'
 #Org2MSP
-docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C ppchannel -n demo-app -c '{"function":"initLedger","Args":[""]}'
+#docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C ppchannel -n demo-app -c '{"function":"initLedger","Args":[""]}'
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
 printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n"
