d44fdd868f39b6f3178a41e769d842deaea13f40
Prashant Prakash <oracle.prashantprakash@gmail.com>
2018-10-22 22:32:33 +0530

    Update demo-chaincode.go
    Rich Query Support and History

--- chaincode/demo-app/demo-chaincode.go
+++ chaincode/demo-app/demo-chaincode.go
@@ -13,6 +13,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
@@ -63,6 +64,10 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 		return s.queryAllProperty(APIstub)
 	} else if function == "changePropertyHolder" {
 		return s.changePropertyHolder(APIstub, args)
+	} else if function == "getHistoryForProperty" {
+		return s.getHistoryForProperty(APIstub, args)
+	} else if function == "queryPropertyByHolder" {
+		return s.queryPropertyByHolder(APIstub, args)
 	}
 	fmt.Println("Invoke failed for unknown function " + function)
 	return shim.Error("Invalid Smart Contract function name")
@@ -167,7 +172,7 @@ func (s *SmartContract) queryAllProperty(APIstub shim.ChaincodeStubInterface) sc
 
 /*
  * The initLedger method *
-Will add test data (4 properties)to our network
+Will add test data (5 properties)to the network
 */
 func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
 	// manually insert entries into the ledger
@@ -225,6 +230,154 @@ func (s *SmartContract) changePropertyHolder(APIstub shim.ChaincodeStubInterface
 	}
 	fmt.Println("End changePropertyHolder")
 	return shim.Success(nil)
+}
+
+/*
+ * getHistoryForProperty function *
+get history for a property
+*/
+func (s *SmartContract) getHistoryForProperty(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+	key := args[0]
+	propertyAsBytes, _ := APIstub.GetState(args[0])
+	if propertyAsBytes == nil {
+		return shim.Error(fmt.Sprintf("Could not locate property: %s", args[0]))
+	}
+	fmt.Println("Property found: %s", args[0])
+	resultsIterator, err := APIstub.GetHistoryForKey(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+	// buffer is a JSON array containing historic values for the property
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// if it is a delete operation on a given key, then we need to set the
+		// corresponding value null. Else, we'll write the response.Value as-is
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+
+		buffer.WriteString(", \"Timestamp\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"IsDelete\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+	fmt.Println("- getHistoryForProperty returning:\n%s\n", buffer.String)
+	return shim.Success(buffer.Bytes())
+}
+
+/*
+ *queryPropertyByHolder*
+ Parametrised Rich Query
+ This function queries for properties based on a pssed in holder
+ This is an example of a parameterised query where the query logic is baked into the chaincode,
+ and accepting a single query parameter (holder)
+ Only available on state databases that support rich query (e.g. CouchDB)
+*/
+func (s *SmartContract) queryPropertyByHolder(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+	if len(args[0]) <= 0 {
+		return shim.Error("Holder must be a non-empty string")
+	}
+	// holder := strings.ToLower(args[0])
+	holder := args[0]
+	// queryString := fmt.Sprintf("{\"selector\":{\"propertyname\":\"property\",\"holder\":\"%s\"}}", holder)
+	queryString := fmt.Sprintf("{\"selector\":{\"holder\":\"%s\"}}", holder)
+
+	queryResults, err := getQueryResultForQueryString(APIstub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
+
+/*
+ *getQueryResultForQueryString*
+Executes the passed in query string.
+Result set is built and returned as a byte array containing JSON results.
+*/
+func getQueryResultForQueryString(APIstub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n:", queryString)
+	resultsIterator, err := APIstub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	buffer, err := constructQueryResponseFromIterator(resultsIterator)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+	return buffer.Bytes(), nil
+
+}
+
+/*
+ *constructQueryResponseFromIterator*
+constructs a JSON array containing query results from a given result iterator
+*/
+func constructQueryResponseFromIterator(resultsIterator shim.StateQueryIteratorInterface) (*bytes.Buffer, error) {
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write it as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+	return &buffer, nil
 }
 
 /*
